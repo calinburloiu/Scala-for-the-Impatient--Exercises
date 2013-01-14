df7b168bd67e59197c2d01b4fb1e49dcb9f28f08
Calin-Andrei Burloiu <calin.burloiu@gmail.com>
2013-01-15 00:11:17 +0200

    Add some exercises from traits chapter.

--- src/exercises/c10_traits/TraitsTest.scala
+++ src/exercises/c10_traits/TraitsTest.scala
@@ -4,6 +4,18 @@ trait Logger {
   def log(msg: String)
 }
 
-class TraitsTest extends App {
+trait CrytoLogger extends Logger {
+  var key: Int = 13
+  
+  abstract override def log(msg: String) { super.log(encrypt(msg)) }
+  
+  def encrypt(str: String) = str.map((c: Char) => (c + key).asInstanceOf[Char])
+}
 
+trait ConsoleLogger extends Logger {
+  def log(msg: String) { println(msg) }
+}
+
+object TraitsTest extends App with ConsoleLogger with CrytoLogger {
+  log("Ana are mere.")
 }
