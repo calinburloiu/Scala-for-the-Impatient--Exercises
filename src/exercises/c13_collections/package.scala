fd508aa8b9c0401eca12362c75353825e94c07bc
Calin-Andrei Burloiu <calin.burloiu@gmail.com>
2013-01-22 13:40:25 +0200

    c13: Add function to compute a stream for factorial numbers.

--- src/exercises/c13_collections/package.scala
+++ src/exercises/c13_collections/package.scala
@@ -2,9 +2,14 @@ package exercises
 
 package object c13_collections {
   def fibo(n: Int) = n match {
-    case 0 => List(0)
-    case 1 => List(0, 1)
+    case 0 => Vector(0)
+    case 1 => Vector(0, 1)
     case _ =>
         (Vector(0, 1) /: (2 to n)) { (v, i) => v :+ (v(i-1) + v(i-2)) }
+  }
+
+  def fiboStream: Stream[Int] = 0 #:: 1 #:: {
+    def f(a: Int, b: Int): Stream[Int] = (a + b) #:: f(b, a + b)
+    f(0, 1)
   }
 }
